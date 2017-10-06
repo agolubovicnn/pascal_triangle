df339079ddd3bab8ea6d453aa4d96caf27f3ee60
Aleksandar Golubovic <agolubovicnn@gmail.com>
2017-10-06 10:15:16 +0200

    Rspec test on calculate method

--- spec/pascal_spec.rb
+++ spec/pascal_spec.rb
@@ -1,26 +1,26 @@
-require_relative '../pascal'
+require_relative '../pascal_main'
 
 describe Pascal do
 
-  describe "#pascal_triangle" do
+  describe "#calculate" do
     it "returns result using row number 0" do
-      expect(Pascal.new.pascal_triangle(0)).to eq(["1"])
+      expect(Pascal.new.calculate(0)).to eq([[1]])
     end
 
     it "returns result using row number 1" do
-      expect(Pascal.new.pascal_triangle(1)).to eq(["1", "1 1"])
+      expect(Pascal.new.calculate(1)).to eq([[1], [1, 1]])
     end
 
     it "returns result using row number 2" do
-      expect(Pascal.new.pascal_triangle(2)).to eq(["1", "1 1", "1 2 1"])
+      expect(Pascal.new.calculate(2)).to eq([[1], [1, 1], [1, 2, 1]])
     end
 
     it "returns result using row number 3" do
-      expect(Pascal.new.pascal_triangle(3)).to eq(["1", "1 1", "1 2 1", "1 3 3 1"])
+      expect(Pascal.new.calculate(3)).to eq([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]])
     end
 
     it "returns result using row number(4)" do
-      expect(Pascal.new.pascal_triangle(4)).to eq(["1", "1 1", "1 2 1", "1 3 3 1", "1 4 6 4 1"])
+      expect(Pascal.new.calculate(4)).to eq([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]])
     end
   end
 end
