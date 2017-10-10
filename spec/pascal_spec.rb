965aad049d24e8f0dfe052bf10dd492c82b7cfc6
Aleksandar Golubovic <agolubovicnn@gmail.com>
2017-10-10 10:42:21 +0200

    Changed rspec test on calculate method

--- spec/pascal_spec.rb
+++ spec/pascal_spec.rb
@@ -8,19 +8,19 @@ describe Pascal do
     end
 
     it "returns result using row number 1" do
-      expect(Pascal.new.calculate(1)).to eq([[1], [1, 1]])
+      expect(Pascal.new.calculate(1)).to eq([[1]])
     end
 
     it "returns result using row number 2" do
-      expect(Pascal.new.calculate(2)).to eq([[1], [1, 1], [1, 2, 1]])
+      expect(Pascal.new.calculate(2)).to eq([[1], [1, 1]])
     end
 
     it "returns result using row number 3" do
-      expect(Pascal.new.calculate(3)).to eq([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]])
+      expect(Pascal.new.calculate(3)).to eq([[1], [1, 1], [1, 2, 1]])
     end
 
     it "returns result using row number(4)" do
-      expect(Pascal.new.calculate(4)).to eq([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]])
+      expect(Pascal.new.calculate(4)).to eq([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]])
     end
   end
 end
